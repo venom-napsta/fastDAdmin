13e18e366fc076baf1b4fa785c951c7410d0d9ac
venom napsta <49985091+venom-napsta@users.noreply.github.com>
2022-10-01 11:52:11 +0200

    Update authSlice.js

--- src/features/slice/authSlice.js
+++ src/features/slice/authSlice.js
@@ -12,7 +12,7 @@ export const login = createAsyncThunk(
     };
     try {
       const res = await http.post(
-        'http://75.119.154.13:5000/login',
+        '/login',
         {
           contact,
           password,
